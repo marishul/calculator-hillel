9c3c6f29233a4939fdbc04dbc0b9aaa8ca8f026c
mariashulyakovskaya <mariashul95@gmail.com>
2020-05-29 12:52:28 +0300

    add .00 after dot for double

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -25,30 +25,30 @@ public class Calculator {
                     System.out.println("Type any 2 numbers to sum (use space between): ");
                     int firstSumNumber = userInput.nextInt();
                     int secondSumNumber = userInput.nextInt();
-                    System.out.println("Result of sum operation is: " + sum(firstSumNumber, secondSumNumber) + "\n\n" + "Play again!");
+                    System.out.println("Result of sum operation is: " + sum(firstSumNumber, secondSumNumber) + "\n\nPlay again!");
                     break;
                 case "2":
                     System.out.println("Type any 2 numbers to sub (use space between): ");
                     int firstSubNumber = userInput.nextInt();
                     int secondSubNumber = userInput.nextInt();
-                    System.out.println("Result of sub operation is: " + sub(firstSubNumber, secondSubNumber) + "\n\n" + "Play again!");
+                    System.out.println("Result of sub operation is: " + sub(firstSubNumber, secondSubNumber) + "\n\nPlay again!");
                     break;
                 case "3":
                     System.out.println("Type any 2 numbers to mul (use space between): ");
                     long firstMulNumber = userInput.nextLong();
                     long secondMulNumber = userInput.nextLong();
-                    System.out.println("Result of mul operation is: " + mul(firstMulNumber, secondMulNumber)+ "\n\n" + "Play again!");
+                    System.out.println("Result of mul operation is: " + mul(firstMulNumber, secondMulNumber) + "\n\nPlay again!");
                     break;
                 case "4":
                     System.out.println("Type any 2 numbers to div (use space between): ");
-                    int firstDivNumber = userInput.nextInt();
-                    int secondDivNumber = userInput.nextInt();
-                    System.out.println("Result of div operation is: " + div(firstDivNumber, secondDivNumber)+ "\n\n" + "Play again!");
+                    double firstDivNumber = userInput.nextDouble();
+                    double secondDivNumber = userInput.nextDouble();
+                    System.out.printf("Result of div operation is: %.2f \n\nPlay again!\n", div(firstDivNumber, secondDivNumber));
                     break;
                 case "5":
                     System.out.println("Type any number to sqrt: ");
                     double sqrtNumber = userInput.nextDouble();
-                    System.out.println("Result of sqrt operation is: " + sqrt(sqrtNumber)+ "\n\n" + "Play again!");
+                    System.out.printf("Result of sqrt operation is: %.2f \n\nPlay again!\n", sqrt(sqrtNumber));
                     break;
             }
         }
@@ -57,18 +57,22 @@ public class Calculator {
     public static int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
     }
+
     public static int sub(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
     }
+
     public static long mul(long firstNumber, long secondNumber) {
-        return firstNumber*secondNumber;
+        return firstNumber * secondNumber;
     }
-    public static int div(int firstNumber, int secondNumber) {
+
+    public static double div(double firstNumber, double secondNumber) {
         if (secondNumber == 0) {
             System.out.println("Can't divide to 0!");
         }
         return firstNumber / secondNumber;
     }
+
     public static double sqrt(double Number) {
         return Math.sqrt(Number);
     }
